96b207644d464a5e7c9d94163095ce36144f42b6
Teodora Iacob <94394424+teoiac@users.noreply.github.com>
2025-01-11 12:43:07 +0200

    Update symtable.h

--- symtable.h
+++ symtable.h
@@ -1,6 +1,5 @@
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
-
 #include <iostream>
 #include <map>
 #include <string>
@@ -9,18 +8,13 @@
 
 using namespace std;
 
-// Simplified ParamList (you can expand this later)
-class ParamList {
-public:
-    vector<pair<string, string>> params; // (type, name)
-};
 
 class IdInfo {
 public:
     string idType; // "var", "func", "class"
     string type;
     string name;
-    ParamList params; // for functions
+    vector<pair<string,string>>* paramList;
     IdInfo() {}
     IdInfo(const string& idType, const string& type, const string& name) : idType(idType), type(type), name(name) {}
 };
@@ -33,7 +27,8 @@ public:
     SymTable(const string& name, SymTable* parent = nullptr) : name(name), parent(parent) {}
     bool existsId(const string& s);
     void addVar(const string& type, const string& name);
-    void addFunc(const string& returnType, const string& name, const ParamList& params);
+    void addFunc(const string& returnType, const string& name, vector<pair<string,string>>* paramList);
+    void addArray(const string& type, const string& name);
     void addClass(const string& name);
     void printVars(ostream& out); //Added ostream for flexibility
     void printTableToFile(const string& filename);
@@ -42,3 +37,4 @@ public:
 };
 
 #endif
+
